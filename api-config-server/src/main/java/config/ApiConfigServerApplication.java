e24ffe7425520de037ec89e123e864388b851413
vishwasraiborde19 <103375323+vishwasraiborde19@users.noreply.github.com>
2022-05-20 16:46:00 +0100

    config changes

--- api-config-server/src/main/java/config/ApiConfigServerApplication.java
+++ api-config-server/src/main/java/config/ApiConfigServerApplication.java
@@ -1,15 +1,11 @@
 package config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
-@Configuration
-@EnableAutoConfiguration
 public class ApiConfigServerApplication {
 
 	public static void main(String[] args) {
